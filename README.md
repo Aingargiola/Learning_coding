a2b1167c3dd56ca5e659fa7dcf5bfd614f3bc36a
Anthony Ingargiola <AIngargiola@Heathers-MBP.lan>
2022-11-16 16:10:47 -0600

    updates

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
  This repository is all about teaching some of the students base code in the bash and python enviroment
 I will try to add descriptive comments to all the code to make it easier to understand and digest to people just learning.
 I am not a coding expert just a hobby i enjoyed and a skill i teach to my cyber security boot camp.
+
+Arthor 
+Anthony Ingargiola
+aingargiola8@gmail.com
--- bash/learn.sh
+++ bash/learn.sh
@@ -3,6 +3,7 @@ echo "Lets do math"
 #This is a variable we call a variable by using $ + variable name
  #Number1=9
 echo "Please enter a number"
+# The read command here is reading the data the user plugs into the command line and will use that number as the variable later in the program
 read Number1
 Number2=11
 
@@ -16,6 +17,7 @@ echo "your total is "  $total
 # echo $Number2
 
 #This is an array were we can put multiple items in a variable and call or only certain ones
+# When counting in an array remember that the computer starts at 0 and counts up from there like here we would have 0, 1, 2
 names=("andrew" "anthony" "ashley")
 
 echo ${names[@]}
