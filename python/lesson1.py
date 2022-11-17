5db89682e9c3a145deea5357b81792e0dce63130
Anthony Ingargiola <AIngargiola@Heathers-MBP.lan>
2022-11-16 20:10:06 -0600

    python

---
+++ python/lesson1.py
@@ -0,0 +1,16 @@
+#How to print to screen in python 
+print("Hissss...")
+print("Hello World!")
+#Print with no variable cause to skip a line
+print()
+print("Anthony")
+# Starting your print statment on a new line
+print("The dog ran across the street.\nHe was chasing a car.")
+# As you can see, the end keyword argument determines the characters the print() function sends to the output once it reaches the end of its positional arguments
+print("My name is", "Python.  ", end="")
+print("Monty Python.")
+# Seperates each variable with declared - in this case but could use others like comma or colon
+print("My", "name", "is", "Monty", "Python.", sep="-")
+print("My", "name", "is", "Monty", "Python.", sep=";")
+# prints out with seperation and end statement with * but does not start on new line
+print("My", "name", "is", sep="_", end="*")
