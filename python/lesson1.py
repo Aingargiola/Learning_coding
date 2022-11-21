a4f9b0563582d0106ac74827e37a8ad612bcecd7
Anthony Ingargiola <AIngargiola@Heathers-MBP.lan>
2022-11-20 19:14:57 -0600

    updates

--- python/lesson1.py
+++ python/lesson1.py
@@ -129,3 +129,9 @@ for i in range(2, 10):
     print("hello")
     pass
 
+
+while True:
+    word = input("You're stuck in an infinite loop!\nEnter the secret word to leave the loop: ")
+    if word == "chupacabra":
+        break
+print("You've successfully left the loop!")
