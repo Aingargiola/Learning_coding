b215760bdef857a9cb70b124c38b485ff92bcd4f
Anthony Ingargiola <AIngargiola@Heathers-MBP.lan>
2022-12-14 15:10:11 -0600

    updates

---
+++ bash/helloworld.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Anthony 
+# Date of latest revision: 12/7/22
+# Purpose: Print a string to the terminal
+# echo "Here is where i would write my code"
+
+
+# Main
+# a=4
+# b=3
+# total=$(( $a + $b ))
+# function add(){
+#    ifconfig
+#     echo $total
+# };    
+
+# add
+
+# function loghistory(){
+#     last -7
+# };
+# echo "Enter your first Name"
+# read name
+# echo "$name the last user are"
+# loghistory
+
+
+#!/bin/bash
+mkdir "directory1" "directory2" "directory3" "Directory4"
+    director=("directory1" "directory2" "directory3" "Directory4")
+ touch ${director[3]} > file.txt
+
+
+
