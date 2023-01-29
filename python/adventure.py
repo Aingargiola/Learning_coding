1b984f90c883e63f9bfe5660d942e4d6e60dd671
unknown <aingargiola8@gmail.com>
2023-01-28 19:06:25 -0600

    updates

--- python/adventure.py
+++ python/adventure.py
@@ -1,5 +1,5 @@
 #This is just a fun text based adventure game that can be played in side the terminal still updating and expading this to make it more indpeth and more intresting uses random number genreation
-#to decide if certain events are affective or not
+#to decide if certain events are affective or not we added some familar charchters to the game to make it more fun and enjoyable
 # Setup
 
 import random
@@ -36,7 +36,7 @@ while True:
         print("You encounter a witch in a house made of candy what do you do?")
         fight = input("fight the witch y/n\n")
         if  fight == "y":
-            print("10 sided dice rolled to see if you beat witch")
+            print("10 sided dice rolled to see if you beat witch you need a 5 or higher")
             number = random.randint(1, 10)
             if number >= 5:
                 print(f"you have defeated the witch with a roll of {number} and escaped the forest with a friendly cat name binx")
@@ -50,14 +50,14 @@ while True:
         castle = input("You enter the castle do you go left or right?\n")
         if castle == "left":
             print("You fall through a trap door and find a hulk. You find a bow and arrows next to you.")
-            troll = input("\nDo you shoot the troll or try to run.  shoot/run\n")
+            troll = input("\nDo you shoot the hulk or try to run.  shoot/run\n")
             if troll == "shoot":
                 arrow = random.randint(1, 10)
                 print("You try and shoot the hulk with 70% accuracy")
                 if arrow <= 7:
                     print("You shoot the hulk and escape the castle")
                     quit()
-                else: print("You try and shoot the hulk and miss and the troll hulksmashes you.")
+                else: print("You try and shoot the hulk and miss and then you get hulksmashed.")
             else: print("You try and run from the hulk but he catches you and throws you in the dungeon.")
         else:  print("You head to the right and find a staircase and start to asscend you get to the top and find the princess.\n")  
         princess = input("Do you try and save her?y/n\n")
