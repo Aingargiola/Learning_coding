07397cfaea476b7f3488844ae43efc1210632c83
unknown <aingargiola8@gmail.com>
2023-02-16 20:38:30 -0600

    updates

--- python/ops11.py
+++ python/ops11.py
@@ -1,5 +1,8 @@
 import psutil
 
-b = ('cpu.txt', 'w')
-f = psutil.cpu_times()
+#file = open('cpu.txt', 'w')
+file2 = psutil.cpu_times()
 print(psutil.cpu_times()) 
+
+
+# c:/Users/ainga/OneDrive/Desktop/teaching-code/python/ops11.py > cpu.txt  This to output ot text file
