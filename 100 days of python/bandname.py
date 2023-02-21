0becbe08bc8f1848748bacc09d4e5ad08806c782
unknown <aingargiola8@gmail.com>
2023-02-21 09:21:46 -0600

    updates

---
+++ 100 days of python/bandname.py
@@ -0,0 +1,7 @@
+#Day 1 off 100 days of python project
+
+print("Lets create a band name for you!")
+city = input("Enter the city you were born: ")
+pet = input("What was your pets name: ")
+print(f"Your band name is {city} {pet}")
+print(f"or your band name could be {pet} {city}")
