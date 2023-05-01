c85aa415dc5bf4a3f052009c4925b67a83fe4af4
Anthony Ingargiola <AIngargiola@Heathers-MacBook-Pro.local>
2023-05-01 09:13:28 -0500

    updates

--- bash/directorymaker.sh
+++ bash/directorymaker.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-mkdir "directory1" "directory2" "directory3" "Directory4"
-    director=("directory1" "directory2" "directory3" "Directory4")
-    cd ${director[@]}    
-    for i  in ${director[@]};
-    do touch test.txt
-    done
+#stlouis-ops-201n3 Class 4 Ops Challenge
+#Write a script that:
+#Creates four directories: dir1, dir2, dir3, dir4
+#Put the names of the four directories in an array
+#References the array variable to create a new .txt file in each directory
+mkdir ~/cyberboot/dir1
+mkdir ~/cyberboot/dir2
+mkdir ~/cyberboot/dir3
+mkdir ~/cyberboot/dir4
+folders=("dir1" "dir2" "dir3" "dir4")
+touch ${folders[0]}/text.txt
+touch ${folders[1]}/text.txt
+touch ${folders[2]}/text.txt
+touch ${folders[3]}/text.txt
