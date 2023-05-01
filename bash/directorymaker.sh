414364510bc036ea6a445a4b373ef21983b5f1da
Anthony Ingargiola <AIngargiola@Heathers-MacBook-Pro.local>
2023-05-01 09:14:29 -0500

    updates

--- bash/directorymaker.sh
+++ bash/directorymaker.sh
@@ -4,10 +4,10 @@
 #Creates four directories: dir1, dir2, dir3, dir4
 #Put the names of the four directories in an array
 #References the array variable to create a new .txt file in each directory
-mkdir ~/cyberboot/dir1
-mkdir ~/cyberboot/dir2
-mkdir ~/cyberboot/dir3
-mkdir ~/cyberboot/dir4
+mkdir dir1
+mkdir dir2
+mkdir dir3
+mkdir dir4
 folders=("dir1" "dir2" "dir3" "dir4")
 touch ${folders[0]}/text.txt
 touch ${folders[1]}/text.txt
