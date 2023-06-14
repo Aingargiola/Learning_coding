5a397155656096d60f3c7550024d78f060e6a43b
Anthony Ingargiola <AIngargiola@Heathers-MacBook-Pro.local>
2023-06-14 16:21:10 -0500

    updates

--- python/ops5.py
+++ python/ops5.py
@@ -1,3 +1,20 @@
+# Write a program that works out whether if a given number is an odd or even number
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+
+
+
+
+
+
+
+
+
 # Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
 
 # It will take your current age as the input and output a message with our time left in this format:
@@ -13,9 +30,10 @@ age = int(input("What is your current age? "))
 
 #Write your code below this line 👇
 
-x = ((90 * 365) - age * 365)
-y = ((90 * 52) - age * 52)
-z = ((90 * 12) - age * 12)
 
 
-print(f"You have {x} days, {y} weeks, and {z} months left.")
+
+
+
+
+
