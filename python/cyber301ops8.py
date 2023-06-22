edb2dfbf65b1a8d084e8c142252e6a4ee2823e30
unknown <aingargiola8@gmail.com>
2023-06-21 20:33:46 -0500

    updates

--- python/cyber301ops8.py
+++ python/cyber301ops8.py
@@ -1,22 +1,10 @@
-# You are going to write a program that automatically prints the solution to the FizzBuzz game.
-
-# Your program should print each number from 1 to 100 in turn.
-
-# When the number is divisible by 3 then instead of printing the number it should print "Fizz".
-
-# When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
-
-#   And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
-
-#Write your code below this row 👇
-
-for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    else:
-        print(number)
-    
+# Your task is very simple here: write a program that uses a for loop to "count mississippi" to five.
+#  Having counted to five, 
+# the program should print to the screen the final message "Ready or not, here I come!"
+import time
+mississippi = 1 
+for mississippi in range(1,6):
+    print(mississippi, "Mississippi")
+    time.sleep(1)
+    pass
+print("Ready or not here  I come")
