0f47ee52e0cc8d8108370d3bccde8a5f27ee5d4a
Anthony Ingargiola <AIngargiola@Heathers-MacBook-Pro.local>
2023-06-30 19:15:51 -0500

    updates

--- python/cyber301ops13.py
+++ python/cyber301ops13.py
@@ -1,3 +1,9 @@
+# Today in Class we are building the childhood game Rock Paper Scissors
+# We are going to import the random function for our code today
+# Bonus objective can you put it on a while loop to play again
+#Write your code below this line:
+
+
 import random
 while True:
     user_action = input("Enter a choice (rock, paper, scissors): ")
