3fc1cb4f7f72bc41d33c8f500875d903a62ceaa6
Anthony Ingargiola <AIngargiola@Heathers-MacBook-Pro.local>
2023-07-11 20:04:07 -0500

    updates

--- python/cyber301ops11students.py
+++ python/cyber301ops11students.py
@@ -1,9 +1,5 @@
 # Today we are going to create a random password generator using for loops and arrays in python
 
-
-
-
-
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -18,18 +14,3 @@ password = []
 
 for char in range(1, nr_letters + 1):
     password.append(random.choice(letters))
-
-for char in range(1, nr_symbols + 1):
-    password.append(random.choice(symbols))
-
-for char in range(1, nr_numbers + 1):
-    password.append(random.choice(numbers))
-
-print(password)
-
-random.shuffle(password)
-
-final_pass = ""
-for char in password:
-    final_pass += char
-print(f"Your password is {final_pass}")
