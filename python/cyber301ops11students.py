e809644bce960d2b7104523f5c200fe82ffc52c3
Anthony Ingargiola <AIngargiola@Heathers-MacBook-Pro.local>
2023-07-12 08:05:28 -0500

    updates

--- python/cyber301ops11students.py
+++ python/cyber301ops11students.py
@@ -1,4 +1,6 @@
 # Today we are going to create a random password generator using for loops and arrays in python
+# Make sure to print you password to the screen
+#Can you randomize your password generated
 
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
