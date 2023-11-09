584beac548487b9b34375f959023a99cac579e0b
unknown <aingargiola8@gmail.com>
2023-11-09 17:54:37 -0600

    updates

--- bash/201ops3.sh
+++ bash/201ops3.sh
@@ -2,14 +2,33 @@
 # Condtinal Statment 
 # This ops challenges is about conditinal stamtment of if else statments and how they work
 # We are going to take a varibale with the number and have the computer tell us if its greater than 5 less than 5 or equals 5
-echo "Enter a number between 1 and 10"
-read number
+# echo "Enter a number between 1 and 10"
+# read number
 
-if (( $number > 5 ))
-    then
-        echo "Number is greater than 5"
-elif (( $number < 5 ))
-    then
-        echo "Number is less than 5"
-else echo "Number is equal to 5"
+# if (( $number > 5 ))
+#     then
+#         echo "Number is greater than 5"
+# elif (( $number < 5 ))
+#     then
+#         echo "Number is less than 5"
+# else echo "Number is equal to 5"
+# fi
+
+
+Variables=$Number
+echo "Please enter a number."
+read Number
+sleep 1
+echo "Your number is $Number"
+sleep 1
+ 
+if (( $Number > 5 ))
+    then 
+        echo "Your number is greater than 5."
+elif (( $Number < 5 ))
+    then 
+        echo "Your number is less than 5."
+else echo "Your number is equal to 5."
 fi
+sleep 1
+echo "Thank you for running my program. Good Bye!"
--- bash/201ops4.sh
+++ bash/201ops4.sh
@@ -4,8 +4,8 @@
 # Stretch goal can you do subtraction, multipliaction or division
 # You will need to declare two variables
 
-num1=5
-num2=7
+num1=6
+num2=3
 
 function add(){
   num3=$(( $num1 + $num2 ))
@@ -13,3 +13,25 @@ function add(){
    
 }
 add
+
+function div(){
+  num4=$(( $num3 * $num1 ))
+  echo " The new total is $num4"
+}
+div
+
+
+
+
+# echo "Enter 2 numbers to divide"
+# read num1
+# read num2
+# #Check if denominator is not zero to avoid division by zero
+# if [$num2 = 0]
+#    then
+#    echo "Error: Division by zero is not allowed."
+# function divide(){
+#     result=$(( $num1 / $num2))
+#     echo $result
+# }
+# divide
