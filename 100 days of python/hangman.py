f0bb0fb62e8327c48c67735b509cf37f7d91aa95
unknown <aingargiola8@gmail.com>
2024-01-12 18:51:30 -0600

    updates

--- 100 days of python/hangman.py
+++ 100 days of python/hangman.py
@@ -3,7 +3,7 @@ word_list = ["aardvark", "baboon", "python", "camel", "chicken", "penguin", "lea
 chosen_word = random.choice(word_list)
 
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 word_length = len(chosen_word)
@@ -13,29 +13,34 @@ total_guess = 0
 missed_guess = 0
 end_of_game = False
 while not end_of_game:
-    guess = input("Guess a letter: ").lower()
-    total_guess += 1
+        guess = input("Guess a letter: ").lower()
+        total_guess += 1
 
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
             
-    if guess in chosen_word:
-        print("Letter Correct")
-    else:
-        print("Letter not correct")
-        missed_guess += 1
+        if guess in chosen_word:
+            print("Letter Correct")
+        else:
+            print("Letter not correct")
+            missed_guess += 1
 
 
-    print(display)
+        print(display)
     
-    if "_" not in display:
-        end_of_game = True
-        print("You Win")
-        print(f"The word was {chosen_word}")
-        print(f"It took you {total_guess} tries")
-    elif missed_guess == 3:
-        end_of_game = True
-        print("Sorry you lost")
+        if "_" not in display:
+            end_of_game = True
+            print("You Win")
+            print(f"The word was {chosen_word}")
+            print(f"It took you {total_guess} tries")
+            
+        elif missed_guess == 3:
+            end_of_game = True
+            print("Sorry you lost")
+            print(f"The word was {chosen_word}")
+            
+
+
 
