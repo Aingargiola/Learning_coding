cbafe07d3daa3ff4bf027440370b634aef6fedf8
unknown <aingargiola8@gmail.com>
2024-06-04 18:30:03 -0500

    updates

--- python/ops11.py
+++ python/ops11.py
@@ -3,6 +3,10 @@ import psutil
 # file = open('cpu.txt', 'w')
 file2 = psutil.cpu_times()
 print(psutil.cpu_times()) 
+print(psutil.cpu_percent()) 
+print(psutil.disk_partitions())  
+print(psutil.net_connections())
+print(psutil.users())
 
 
 # c:/Users/ainga/OneDrive/Desktop/teaching-code/python/ops11.py > cpu.txt  
