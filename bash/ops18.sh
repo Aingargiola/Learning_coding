677ae5f8d5a0525d2d73c54bc8382f165c2a14ec
Anthony Ingargiola <aingargiola8@gmail.com>
2024-09-23 19:35:16 -0500

    Updates

--- bash/ops18.sh
+++ bash/ops18.sh
@@ -2,3 +2,10 @@
 
 # Today's challenges is to create a script in bash that naviagetes to the document directory and list the files in there
 # Then ask the user to create or edit a file and opens the file up in the terminal
+
+cd Document
+ls
+
+read file
+
+nano $file
